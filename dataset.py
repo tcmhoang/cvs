9fa01bf02fcc9a3aaf4b2eaf8ed7d7926d53ec8b
Conrad Hoang <conrad@sprqpt.com>
2026-03-23 06:54:07 -0400

    Impl ground work for loading model

--- dataset.py
+++ dataset.py
@@ -1,2 +1,69 @@
-def get_loaders():
-    pass
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import Compose, transforms
+from dio import List, Tuple
+import torchvision
+
+
+def get_img_train_transform(
+    sqr_sz: int, crop_sz: int, norm_mean: List[float], norm_std: List[float]
+) -> Compose:
+    return torchvision.transforms.Compose(
+        [
+            transforms.Resize((sqr_sz, sqr_sz)),
+            transforms.CenterCrop(crop_sz),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=norm_mean, std=norm_std),
+        ]
+    )
+
+
+def get_img_test_transform(
+    sqr_sz: int, crop_sz: int, norm_mean: List[float], norm_std: List[float]
+) -> Compose:
+    return transforms.Compose(
+        [
+            transforms.Resize((sqr_sz, sqr_sz)),
+            transforms.CenterCrop(crop_sz),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=norm_mean, std=norm_std),
+        ]
+    )
+
+
+def get_img_sets(
+    train_dir_w_transfomer: Tuple[str, Compose],
+    test_dir_w_transformer: Tuple[str, Compose],
+) -> Tuple[Dataset, Dataset]:
+    return (
+        torchvision.datasets.ImageFolder(
+            root=train_dir_w_transfomer[0], transform=train_dir_w_transfomer[1]
+        ),
+        torchvision.datasets.ImageFolder(
+            root=test_dir_w_transformer[0], transform=test_dir_w_transformer[1]
+        ),
+    )
+
+
+def get_loaders(
+    train_set_w_batch_zs: Tuple[Dataset, int],
+    test_set_w_batch_sz: Tuple[Dataset, int],
+    num_workers: int,
+):
+    return (
+        DataLoader(
+            train_set_w_batch_zs[0],
+            batch_size=train_set_w_batch_zs[1],
+            shuffle=True,
+            num_workers=num_workers,
+        ),
+        DataLoader(
+            test_set_w_batch_sz[0],
+            batch_size=test_set_w_batch_sz[1],
+            shuffle=False,
+            num_workers=num_workers,
+        ),
+    )
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import torch
 import dio
+import dataset
 import config
 
 
 def main():
 
+    print("CUDA CHECK")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    print("POP DS")
+    # TODO: Load from the nw src
     failed_populateds = dio.prepare(
         {"lzupsd": None, "vsd": None}, config.CACHE_DIR, config.TRAIN_DIR
     )
@@ -14,11 +18,42 @@ def main():
     if len(failed_populateds) != 0:
         print("Failed to prepare", " ".join(failed_populateds))
 
+    print("CATS DS")
     dio.cats(
         config.TEST_PERC,
         config.EVAL_PERC,
         (config.TRAIN_DIR, config.EVAL_DIR, config.TEST_DIR),
     )
+
+    print("PREPARE DS")
+    train_set, test_set = dataset.get_img_sets(
+        (
+            config.TRAIN_DIR,
+            dataset.get_img_train_transform(
+                config.IMAGE_SZ,
+                config.CROP_SZ,
+                config.NORMALIZE_MEAN,
+                config.NORMALIZE_STD,
+            ),
+        ),
+        (
+            config.TEST_DIR,
+            dataset.get_img_test_transform(
+                config.IMAGE_SZ,
+                config.CROP_SZ,
+                config.NORMALIZE_MEAN,
+                config.NORMALIZE_STD,
+            ),
+        ),
+    )
+
+    train_loader, test_loader = dataset.get_loaders(
+        (train_set, config.BATCH_SZ),
+        (test_set, config.TEST_BATCH_SZ),
+        config.NUM_WORKERS,
+    )
+
+    print("INIT MODEL")
 
 
 if __name__ == "__main__":
