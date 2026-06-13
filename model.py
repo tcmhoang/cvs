4807861c9e2aea443316de09fc5978d0953cb05b
Conrad Hoang <conrad@sprqpt.com>
2026-06-13 02:02:33 -0400

    Add linear proj net

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
-from typing import Any, Protocol, cast
+from typing import Any, Callable, Protocol, cast
 
 import torch
 from timm.models.vision_transformer import VisionTransformer
 from torch import Tensor, device, hub, nn
+
+import config
 
 
 class DINOv2ViT(Protocol):
@@ -13,19 +15,35 @@ class DINOv2ViT(Protocol):
     def to(self, device: device) -> "DINOv2ViT": ...
 
 
-def io_get_model(dev: device) -> VisionTransformer:
+def model_freeze_backbone(base: DINOv2ViT) -> None:
+    for param in base.parameters():
+        param.requires_grad = False
+        pass
+    pass
+
+
+def model_pft(base: DINOv2ViT) -> None:
+    model_freeze_backbone(base)
+
+    for param in base.blocks[-1].parameters():
+        param.requires_grad = True
+        pass
+
+    for param in base.norm.parameters():
+        param.requires_grad = True
+        pass
+
+    pass
+
+
+def io_get_model(
+    dev: device, mod: Callable[[DINOv2ViT], None] = model_pft
+) -> VisionTransformer:
     dinov2_model = cast(
         DINOv2ViT, hub.load("facebookresearch/dinov2", "dinov2_vits14")
     ).to(dev)
 
-    for param in dinov2_model.parameters():
-        param.requires_grad = False
-
-    for param in dinov2_model.blocks[-1].parameters():
-        param.requires_grad = True
-
-    for param in dinov2_model.norm.parameters():
-        param.requires_grad = True
+    mod(dinov2_model)
 
     return cast(VisionTransformer, dinov2_model)
 
@@ -43,32 +61,59 @@ class GeM(nn.Module):
 
 
 class RetrievalNet(nn.Module):
-    def __init__(self, model: VisionTransformer, gemp: float, embeding_dim=384) -> None:
+    def __init__(
+        self,
+        model: VisionTransformer,
+        gemp=config.GEM_P,
+        embeding_dim=config.EMBEDDING_DIM,
+    ) -> None:
         super().__init__()
 
         self.model = model
         self.gem = GeM(p=gemp)
 
         self.fc = nn.Sequential(
-            nn.Linear(384, 512), nn.GELU(), nn.Linear(512, embeding_dim)
+            nn.Linear(384, config.MLP_HEAD_DIM),
+            nn.ReLU(),
+            nn.Linear(config.MLP_HEAD_DIM, embeding_dim),
         )
         pass
 
     def forward(self, x: Tensor) -> Tensor:
-        fdicts = self.model.forward_features(x)
+        with torch.no_grad():
+            fdicts = self.model.forward_features(x)
 
-        if isinstance(fdicts, dict):
-            patches = fdicts.get("x_norm_patchtokens")
-            if patches is None:
-                features = fdicts.get("x_norm")
-                patches = cast(Tensor, features)[:, 1:, :]  # [Batch, 256, 384]
+            if isinstance(fdicts, dict):
+                patches = fdicts.get("x_norm_patchtokens")
+                if patches is None:
+                    features = fdicts.get("x_norm")
+                    patches = cast(Tensor, features)[:, 1:, :]  # [Batch, 256, 384]
+                    pass
+            else:
+                features = fdicts
+                patches = features[:, 1:, :]  # [Batch, 256, 384]
                 pass
-        else:
-            features = fdicts
-            patches = features[:, 1:, :]  # [Batch, 256, 384]
+
             pass
 
         pooled = self.gem(patches)
         embeddings = self.fc(pooled)
 
         return nn.functional.normalize(pooled + embeddings, p=2, dim=1)
+
+
+class LinearProjectionNet(nn.Module):
+    def __init__(self, model: VisionTransformer, embedding_dim=config.EMBEDDING_DIM):
+        super().__init__()
+        self.model = model
+        self.head = nn.Linear(384, embedding_dim)
+        pass
+
+    def forward(self, x):
+        with torch.no_grad():
+            fdicts = self.model.forward_features(x)
+            clstokens = fdicts["x_norm_clstoken"]  # type: ignore
+
+        projected_features = self.head(clstokens)
+
+        return nn.functional.normalize(projected_features, p=2, dim=1)
